b4a3f682d29fee0ee2f836ce146c3b919b4fca5f
Stephen O <stephen.oveson@gmail.com>
2019-12-11 02:45:29 -0700

    change for heroku

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const { addUser, removeUser, getUser, getUsersInRoom } = require('./users')
 
 const PORT = process.env.PORT || 5000;
 
-const router = require('./router')
+// const router = require('./router')
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
 app.use(cors());
-app.use(router);
+// app.use(router);
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
