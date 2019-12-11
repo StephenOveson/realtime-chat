a759a736c1bb7ce2dca332bfcad6ddc15daf8944
Stephen O <stephen.oveson@gmail.com>
2019-12-11 02:12:06 -0700

    changes to procfile

---
+++ client/src/components/UsersInRoom/UsersInRoom.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import onlineIcon from '../../icons/onlineIcon.png'
+
+import './UsersInRoom.css'
+
+const UsersInRoom = ({ users }) => (
+    <>
+        <div className="textContainer">
+            {
+                users
+                    ? (
+                        <div>
+                            <h1>People currently chatting:</h1>
+                            <div className="activeContainer">
+                                <h2>
+                                    {users.map(({ name }) => (
+                                        <div key={name} className="activeItem">
+                                            {name}
+                                            <img alt="Online Icon" src={onlineIcon} />
+                                        </div>
+                                    ))}
+                                </h2>
+                            </div>
+                        </div>
+                    )
+                    : null
+            }
+        </div>
+    </>
+)
+
+export default UsersInRoom
